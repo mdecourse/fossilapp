f1e255b7f2bdca0c9272334c87b391f59bcfaef4
mdecourse <mdecourse@gmail.com>
2021-06-06 18:11:44 +0800

    finish the first fossilapp version

--- fossilapp.py
+++ fossilapp.py
@@ -2,6 +2,7 @@ from flask import Flask, request
 
 # for generating secret_key
 import os
+import time
 
 app = Flask(__name__)
 
@@ -34,7 +35,78 @@ def genAccount():
     # get user input account and password
     account = request.form["account"]
     password = request.form["password"]
+    
+    # repository location path
+    path = "/home/yen/repository/u/"
+    
+    output = ""
+    
+    # copy fossil repository from template.fossil
+    command1 = "cp " + path + "template.fossil "  + path + account + ".fossil"
+    
+    output += command1 + "<br />"
+    
+    # add account as the repository user which need to force user use student number as account
+    command2 = "fossil user new " + account + " " + account + "@gm.nfu.edu.tw " + password + " -R " + path + account + ".fossil"
+    
+    output += command2 + "<br />"
+    
+    # set account to be administrator which capability is "s" (setup)
+    command3 = "fossil user capabilities " + account + " s"  + " -R " + path + account + ".fossil"
+    
+    output += command3 + "<br />"
+    
+    # change the origin "cd" account capabilities to none which is a vacant string " "
+    command4 = "fossil user capabilities  cd  '' "  + " -R " + path + account + ".fossil"
+    
+    output += command4 + "<br />"
+    
+    output += "<br\><br \>"
+    
+    # change directory to user repository path
+    os.system("cd " + path)
+    
+    # execute command1
+    os.system(command1)
+    
+    # wait for 0.1 second
+    time.sleep(0.1)
+    
+    # execute command2
+    try:
+        os.system(command2)
+        output += "command2 completed <br />"
+    except:
+        output += "command2 failed<br \>"
+    
+    # wait for 0.1 second
+    time.sleep(0.1)
+    
+    # execute command3
+    try:
+        os.system(command3)
+        output += "command3 completed <br />"
+    except:
+        output += "command3 failed <br \>"
+    
+    # wait for 0.1 second
+    time.sleep(0.1)
+    
+    # execute command4
+    try:
+        os.system(command4)
+        output += "command4 completed <br />"
+    except:
+        output += "command4 failed<br \>"
+    
+    # return command for debug
+    #return output
+    
+    return "account: " + account + " created!"
+    
+    '''
     return "account:" + account+"<br />" \
     + "password:" + password
+    '''
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
